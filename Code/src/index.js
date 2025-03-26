25c85d15980d7b31e0a0905c58324327f570fa63
Islam-Hamad <islamses@hotmail.com>
2025-03-26 11:32:31 +0300

    Updates

--- Code/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import PortfolioApp from './PortfolioApp';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<PortfolioApp />);
